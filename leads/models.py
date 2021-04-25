9c0f655674e227830677f20ecdc882b13ace5f03
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-04-25 22:28:00 +0500

    added agent model n created relations

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,17 @@
-from django.db.models import Model
-from django.db.models.fields import CharField, IntegerField
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class Lead(Model):
-    first_name = CharField(max_length= 20)
-    last_name = CharField(max_length= 20)
-    age = IntegerField(default=0)
+class User(AbstractUser):
+    pass
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE, default='')
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
