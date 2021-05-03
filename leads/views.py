1f2f0dd8db295cf129014eb075e335f28e872ca0
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-05-03 10:10:33 +0500

    added static route

--- leads/views.py
+++ leads/views.py
@@ -42,35 +42,3 @@ class LeadDeletePage(generic.DeleteView):
 
     def get_success_url(self):
         return reverse('leads:lead_list')
-
-
-# def lead_delete(request, pk):
-#     lead = Lead.objects.get(id=pk)
-#     lead.delete()
-#     return redirect("/leads")
-
-# def lead_create(request):
-#     form = LeadForm()
-
-#     if request.method == "POST":
-#         form = LeadForm(request.POST)
-#         if form.is_valid():
-#             first_name = form.cleaned_data['first_name']
-#             last_name = form.cleaned_data["last_name"]
-#             age = form.cleaned_data["age"]
-#             agent = form.cleaned_data["agent"]
-
-#             Lead.objects.create(
-#                 first_name=first_name,
-#                 last_name=last_name,
-#                 age= age,
-#                 agent= agent
-#                  )
-#             print(f"Lead {first_name} {last_name} added to db.")
-#             return redirect("/leads")
-
-#     context = {
-#         "form": form
-#     }
-
-#     return render(request, "leads/lead_create.html", context)
