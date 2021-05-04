9a3580aa76ddd1685ccc01550cd99bd37bca6848
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-05-04 08:42:39 +0500

    signal authentication before save

--- leads/models.py
+++ leads/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.deletion import CASCADE
-
+from django.db.models.signals import post_save
 
 class User(AbstractUser):
     pass
@@ -27,3 +26,10 @@ class Agent(models.Model):
 
     def __str__(self) :
         return self.user.username
+    
+def post_user_created_signal(sender,instance,created,**kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    
+
+post_save.connect(post_user_created_signal,sender=User)
