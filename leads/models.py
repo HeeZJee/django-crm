87489eaac25441e4881721838b548607b76718e6
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-05-08 07:16:53 +0500

    added check for role in user model

--- leads/models.py
+++ leads/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.base import Model
 from django.db.models.signals import post_save
 
 class User(AbstractUser):
-    pass
+    is_organisor = models.BooleanField(default=True)
+    is_agent = models.BooleanField(default=False)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
