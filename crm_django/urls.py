d4cc1a80a31bce5ff5f13a94f031dc3d323953e6
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-05-10 01:53:05 +0500

    added password reset

--- crm_django/urls.py
+++ crm_django/urls.py
@@ -4,7 +4,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin 
 from django.urls import path,include
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +20,10 @@ urlpatterns = [
     path('signup/',SignupView.as_view(), name="signup" ),
     path('login/',LoginView.as_view(), name="login" ),
     path('logout/',LogoutView.as_view(), name="logout" ),
+    path('reset-password/',PasswordResetView.as_view(), name="reset_password" ),
+    path('password-reset-done/',PasswordResetDoneView.as_view(), name="password_reset_done" ),
+    path('password-reset-confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(), name="reset_password_confirm" ),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(), name="password_reset_complete" ),
 ]
 
 if settings.DEBUG:
